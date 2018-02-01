3a1b097d466f998b6368a9173870f6afc0d5b0fe
tsokalo <ievgenii.tsokalo@tu-dresden.de>
2018-02-01 10:14:37 +0100

    cleaned file system

--- comp/dep/googletest/build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/mtomoskozi/DATADRIVE1/wp_second/fbcomp/dep/googletest/googlemock
-# Build directory: /media/mtomoskozi/DATADRIVE1/wp_second/fbcomp/dep/googletest/build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- comp/dep/googletest/build/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/mtomoskozi/DATADRIVE1/wp_second/fbcomp/dep/googletest/googletest
-# Build directory: /media/mtomoskozi/DATADRIVE1/wp_second/fbcomp/dep/googletest/build/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
